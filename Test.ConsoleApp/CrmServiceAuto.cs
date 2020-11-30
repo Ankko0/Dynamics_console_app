9cc5edeb268acf7d41deee19feed623981a6da7b
Ankko0 <44235987+Ankko0@users.noreply.github.com>
2020-11-30 14:39:30 +0300

    Initial commit

---
+++ Test.ConsoleApp/CrmServiceAuto.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Linq;
+using Dynamics.ConsoleApp.Entities;
+
+namespace Test.ConsoleApp
+{
+    class CrmServiceAuto
+    {
+        public void AddCommunication(IOrganizationService service,  EntityCollection contactsWithoutComunication)
+        {
+            foreach (var contact in contactsWithoutComunication.Entities.Select(e=>e.ToEntity<Contact>()))
+            {
+                if (contact.Telephone1 != null  && contact.EMailAddress1 == null )
+                {
+                    var newCommunicationId =  service.Create(new nav_communication());
+                    var newCommunication = service.Retrieve(nav_communication.EntityLogicalName, newCommunicationId, new ColumnSet(true)).ToEntity<nav_communication>();
+                    newCommunication.nav_phone = contact.Telephone1;
+                    newCommunication.nav_name = contact.FullName;
+                    newCommunication.nav_main = true;
+                    newCommunication.nav_type = nav_communication_nav_type.Telefon;
+                    newCommunication.nav_contactid.Id = contact.Id;
+
+                    service.Update(newCommunication);
+
+                }
+
+                if (contact.Telephone1 == null && contact.EMailAddress1 != null)
+                {
+                    var newCommunicationId = service.Create(new nav_communication());
+                    var newCommunication = service.Retrieve(nav_communication.EntityLogicalName, newCommunicationId, new ColumnSet(true)).ToEntity<nav_communication>();
+                    newCommunication.nav_email = contact.EMailAddress1;
+                    newCommunication.nav_name = contact.FullName;
+                    newCommunication.nav_main = false;
+                    newCommunication.nav_type = nav_communication_nav_type.E_mail;
+                    newCommunication.nav_contactid.Id = contact.Id;
+
+                    service.Update(newCommunication);
+
+                }
+
+                if (contact.Telephone1 != null && contact.EMailAddress1 != null)
+                {
+                    var newCommunicationId = service.Create(new nav_communication());
+                    var newCommunication = service.Retrieve(nav_communication.EntityLogicalName, newCommunicationId, new ColumnSet(true)).ToEntity<nav_communication>();
+                    newCommunication.nav_phone = contact.Telephone1;
+                    newCommunication.nav_name = contact.FullName + " Phone";
+                    newCommunication.nav_main = true;
+                    newCommunication.nav_type = nav_communication_nav_type.Telefon;
+                    newCommunication.nav_contactid.Id = contact.Id;
+
+                    service.Update(newCommunication);
+
+                    newCommunicationId = service.Create(new nav_communication());
+                    newCommunication = service.Retrieve(nav_communication.EntityLogicalName, newCommunicationId, new ColumnSet(true)).ToEntity<nav_communication>();
+                    newCommunication.nav_email = contact.EMailAddress1;
+                    newCommunication.nav_name = contact.FullName + "E-mail";
+                    newCommunication.nav_main = false;
+                    newCommunication.nav_type = nav_communication_nav_type.E_mail;
+                    newCommunication.nav_contactid.Id = contact.Id;
+
+                    service.Update(newCommunication);
+
+                }
+            }
+        }
+    }
+}
---
+++ Test.ConsoleApp/Program.cs
@@ -0,0 +1,80 @@
+﻿using Dynamics.ConsoleApp.Entities;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Linq;
+
+namespace Dynamics.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var connectionString = "AuthType=OAuth;Username=admin@awesome16.onmicrosoft.com;Password=Maxim!@#;Url=https://trial16.crm4.dynamics.com;RequireNewInstance=true;AppId=51f81489-12ee-4a9e-aaae-a2591f45987d;RedirectUri=app://58145B91-0C36-4500-8554-080854F2AC97";
+            CrmServiceClient client = new CrmServiceClient(connectionString);
+            if (client.LastCrmException != null)
+            {
+                Console.WriteLine(client.LastCrmException);
+            }
+
+            var service = (IOrganizationService)client;
+            QueryExpression query = new QueryExpression(nav_communication.EntityLogicalName);
+            query.ColumnSet = new ColumnSet(nav_communication.Fields.nav_contactid,
+                nav_communication.Fields.nav_email,
+                nav_communication.Fields.nav_phone,
+                nav_communication.Fields.nav_main,
+                nav_communication.Fields.nav_type);
+            query.NoLock = true;
+            query.TopCount = 20;
+            query.Criteria.AddCondition(nav_communication.Fields.nav_main, ConditionOperator.Equal, true);
+            var result = client.RetrieveMultiple(query);
+
+            foreach (var entity in result.Entities.Select(e => e.ToEntity<nav_communication>()))
+            {
+                var contact = client.Retrieve(Contact.EntityLogicalName, entity.nav_contactid.Id, new ColumnSet(Contact.Fields.Telephone1, Contact.Fields.EMailAddress1)).ToEntity<Contact>();
+                if (entity.nav_type == nav_communication_nav_type.Telefon && contact.Telephone1 == null)
+                {
+                    contact.Telephone1 = entity.nav_phone;
+                    client.Update(contact);
+                }
+                if (entity.nav_type == nav_communication_nav_type.E_mail && contact.EMailAddress1 == null)
+                {
+                    contact.EMailAddress1 = entity.nav_email;
+                    client.Update(contact);
+                }
+
+                //Console.WriteLine($"{entity.Id} {entity.GetAttributeValue<string>("name") }");
+            }
+
+            /*QueryExpression query2 = new QueryExpression(Contact.EntityLogicalName);
+            query.Criteria = new FilterExpression(LogicalOperator.And);
+            query.Criteria.AddCondition(new ConditionExpression("AttributeChildEntityEntityReferenceToParentEntity", ConditionOperator.Equal, new Guid("GuidOfParententity")));
+
+            RetrieveEntityRequest retrieveBankAccountEntityRequest = new RetrieveEntityRequest
+
+            {
+
+                EntityFilters = EntityFilters.Relationships,
+
+                LogicalName = "Contact"
+
+            };
+
+            RetrieveEntityResponse retrieveBankAccountEntityResponse = (RetrieveEntityResponse)service.Execute(retrieveBankAccountEntityRequest);
+
+            var oneToNRelationships = retrieveBankAccountEntityResponse.EntityMetadata.OneToManyRelationships;
+
+            foreach (var oneToNRelationship in oneToNRelationships)
+
+            {
+                int temp123 = 5;
+                // your code
+
+            }*/
+
+            Console.Read();
+        }
+    }
+}
